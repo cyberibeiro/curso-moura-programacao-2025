91397fb08bf1e8a9bdfe3975686795ac74c271a7
cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-09-11 12:49:51 -0300

    aprendendo switch

--- Logica/Voto/Program.cs
+++ Logica/Voto/Program.cs
@@ -19,17 +19,15 @@ namespace Voto
     {
         static void Main(string[] args)
         {
-            #region "****Solução com voto do tipo int e Estrutura de Seleção (switch) com loop****"
-
             const int CANDIDATO1 = 1, CANDIDATO2 = 2, CANDIDATO3 = 3, CANDIDATO4 = 4, NULO = 5, BRANCO = 6;
-            int votoCandidatos = 0;
-            int votoNulo = 0;
-            int votoBranco = 0;
-            int votoInvalido = 0;
+
             string continuar = "s";
+            int contadorVotosEmCandidato = 0, contadorVotosNulos = 0, contadorVotosBrancos = 0, contadorVotosInvalidos = 0;
 
-            do
+            //Console.Write("Deseja votar (s-sim n-não)?");
+            //continuar = Console.ReadLine();
 
+            while (continuar == "s")
             {
                 Console.WriteLine("--Voto--\n");
                 Console.WriteLine("1,2,3,4 votos para os respectivos candidatos");
@@ -37,146 +35,126 @@ namespace Voto
                 Console.WriteLine("6 voto em branco");
 
                 Console.Write("Voto:");
+                #region "****Solução com voto do tipo int e Estrutura de Seleção (switch)****"
+
                 int voto = int.Parse(Console.ReadLine());
 
-                switch (votos)
+                switch (voto)
                 {
                     case CANDIDATO1:
                     case CANDIDATO2:
                     case CANDIDATO3:
                     case CANDIDATO4:
                         Console.WriteLine("Voto contabilizado com sucesso...");
-                        votoCandidatos++;
+                        contadorVotosEmCandidato++;
                         break;
                     case NULO:
                         Console.WriteLine("Voto nulo contabilizado...");
-                        votoNulo++;
+                        contadorVotosNulos++;
                         break;
                     case BRANCO:
                         Console.WriteLine("Voto branco contabilizado...");
-                        votoBranco++;
+                        contadorVotosBrancos++;
                         break;
                     default:
                         Console.WriteLine("Voto inválido...");
-                        votoInvalido++;
+                        contadorVotosInvalidos++;
                         break;
-
-                    Console.WriteLine("\nGostaria de inserir mais um voto (s/n)? ");
-
-                        Console.Clear();
                 }
-            }while (continuar == "s");
 
-            Console.WriteLine($"\n--Votos contabilizados--");
+                #endregion
 
-            Console.WriteLine($"\nTotal de votos para candidatos: {votoCandidatos}");
-            Console.WriteLine($"Total de votos nulos: {votoNulo}");
-            Console.WriteLine($"Total de votos em branco: {votoBranco}");
-            Console.WriteLine($"Total de votos inválidos: {votoInvalido}");
+                Console.Write("Deseja continuar votando (s-sim n-não)?");
+                continuar = Console.ReadLine();
 
-            #endregion
-
-            #region "Solução com voto do tipo int e ED encadeada e operadores lógicos - E"
-            /*            
-            int voto = int.Parse(Console.ReadLine());
-
-            //if (voto > 0 && voto < 5)
-            if (voto >= CANDIDATO1 && voto <= CANDIDATO4)
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == NULO)
-                Console.WriteLine("Voto nulo contabilizado...");
-            else if (voto == BRANCO)
-                Console.WriteLine("Voto branco contabilizado...");
-            else
-                Console.WriteLine("Voto inválido...");
-            */
-            #endregion
-
-            #region "Solução com voto do tipo int e ED encadeada e operadores lógicos - OU"
-
-            /*
-            int voto = int.Parse(Console.ReadLine());
-
-            if (voto == 1 || voto == 2 || voto == 3 || voto == 4)
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == 5)
-                Console.WriteLine("Voto nulo contabilizado...");
-            else if (voto == 6)
-                Console.WriteLine("Voto branco contabilizado...");
-            else
-                Console.WriteLine("Voto inválido...");
-            */
-            #endregion
-
-            #region "****Solução com voto do tipo int e Estrutura de Seleção (switch)****"
-
-            int voto = int.Parse(Console.ReadLine());
-
-            switch (voto)
-            {
-                case CANDIDATO1:
-                case CANDIDATO2:
-                case CANDIDATO3:
-                case CANDIDATO4:
-                    Console.WriteLine("Voto contabilizado com sucesso...");
-                    break;
-                case NULO:
-                    Console.WriteLine("Voto nulo contabilizado...");
-                    break;
-                case BRANCO:
-                    Console.WriteLine("Voto branco contabilizado...");
-                    break;
-                default:
-                    Console.WriteLine("Voto inválido...");
-                    break;
+                Console.Clear();
             }
 
-            #endregion
-
-            #region "Solução com voto do tipo int e ED encadeada"
-            /*
-            int voto = int.Parse(Console.ReadLine());
-
-            if (voto == 1)
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == 2)
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == 3)
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == 4)
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == 5)
-                Console.WriteLine("Voto nulo contabilizado...");
-            else if (voto == 6)
-                Console.WriteLine("Voto branco contabilizado...");
-            else
-                Console.WriteLine("Voto inválido...");
-            */
-            #endregion
-
-            #region "Solução com voto do tipo string e ED encadeada"
-            /*
-            string voto = Console.ReadLine();
-
-            if (voto == "1")
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == "2")
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == "3")
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == "4")
-                Console.WriteLine("Voto contabilizado com sucesso...");
-            else if (voto == "5")
-                Console.WriteLine("Voto nulo contabilizado...");
-            else if (voto == "6")
-                Console.WriteLine("Voto branco contabilizado...");
-            else
-                Console.WriteLine("Voto inválido...");
-            */
-            #endregion
+            Console.WriteLine("--Votos--");
+            Console.WriteLine($"Voto(s) contabilizado(s) com sucesso: {contadorVotosEmCandidato}");
+            Console.WriteLine($"Voto(s) nulo(s) contabilizado(s): {contadorVotosNulos}");
+            Console.WriteLine($"Voto(s) branco(s) contabilizado(s): {contadorVotosBrancos}");
+            Console.WriteLine($"Voto(s) inválido(s) contabilizado(s): {contadorVotosInvalidos}");
+            Console.WriteLine($"Total de votos contabilizados: {contadorVotosEmCandidato + contadorVotosNulos + contadorVotosBrancos + contadorVotosInvalidos}");
 
             Console.WriteLine("\nPressione ENTER para encerrar");
             Console.ReadLine();
+
         }
     }
 }
+
+
+#region "Solução com voto do tipo int e ED encadeada e operadores lógicos - E"
+/*            
+int voto = int.Parse(Console.ReadLine());
+
+//if (voto > 0 && voto < 5)
+if (voto >= CANDIDATO1 && voto <= CANDIDATO4)
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == NULO)
+    Console.WriteLine("Voto nulo contabilizado...");
+else if (voto == BRANCO)
+    Console.WriteLine("Voto branco contabilizado...");
+else
+    Console.WriteLine("Voto inválido...");
+*/
+#endregion
+
+#region "Solução com voto do tipo int e ED encadeada e operadores lógicos - OU"
+
+/*
+int voto = int.Parse(Console.ReadLine());
+
+if (voto == 1 || voto == 2 || voto == 3 || voto == 4)
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == 5)
+    Console.WriteLine("Voto nulo contabilizado...");
+else if (voto == 6)
+    Console.WriteLine("Voto branco contabilizado...");
+else
+    Console.WriteLine("Voto inválido...");
+*/
+#endregion
+
+#region "Solução com voto do tipo int e ED encadeada"
+/*
+int voto = int.Parse(Console.ReadLine());
+
+if (voto == 1)
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == 2)
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == 3)
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == 4)
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == 5)
+    Console.WriteLine("Voto nulo contabilizado...");
+else if (voto == 6)
+    Console.WriteLine("Voto branco contabilizado...");
+else
+    Console.WriteLine("Voto inválido...");
+*/
+#endregion
+
+#region "Solução com voto do tipo string e ED encadeada"
+/*
+string voto = Console.ReadLine();
+
+if (voto == "1")
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == "2")
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == "3")
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == "4")
+    Console.WriteLine("Voto contabilizado com sucesso...");
+else if (voto == "5")
+    Console.WriteLine("Voto nulo contabilizado...");
+else if (voto == "6")
+    Console.WriteLine("Voto branco contabilizado...");
+else
+    Console.WriteLine("Voto inválido...");
+*/
+#endregion
